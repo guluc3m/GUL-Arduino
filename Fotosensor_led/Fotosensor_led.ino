7063e5082b0fb20565bb2f50e61713a66e6aa1d6
PaquitoElChocolatero <mr.booth8@gmail.com>
2018-10-05 14:57:27 +0200

    Arduino

--- Fotosensor_led/Fotosensor_led.ino
+++ Fotosensor_led/Fotosensor_led.ino
@@ -8,18 +8,19 @@ int value;          // Store value from photoresistor (0-1023)
 void setup(){
  pinMode(ledPin, OUTPUT);  // Set lepPin - 9 pin as an output
  pinMode(pResistor, INPUT);// Set pResistor - A0 pin as an input (optional)
+Serial.begin(9600);
 }
 
 void loop(){
   value = analogRead(pResistor);
-  
+  Serial.println(value);
   //You can change value "25"
-  if (value > 50){
+  if (value > 100){
     digitalWrite(ledPin, LOW);  //Turn led off
   }
   else{
     digitalWrite(ledPin, HIGH); //Turn led on
   }
 
-  delay(500); //Small delay
+  delay(50); //Small delay
 }
--- Persiana/Fotosensor_servo/Fotosensor_servo.ino
+++ Persiana/Fotosensor_servo/Fotosensor_servo.ino
@@ -1,29 +1,39 @@
 #include <Servo.h>;
 
-const int pResistor = A0; // Photoresistor at Arduino analog pin A0
+const int pResistor = A0;// Photoresistor at Arduino analog pin A0
+ int pinLED = 8;
 Servo servo;              // Servo declaration for later use
 
 //Variables
 int value;          // Store value from photoresistor (0-1023)
 
 void setup(){
- pinMode(pResistor, INPUT);// Set pResistor - A0 pin as an input (optional)
- servo.attach(1); //Servo attached to pin 1 digital
+  
+ pinMode(pResistor, INPUT);// Set pResistor - A0 pin as an input
+ pinMode(pinLED, OUTPUT); // Set the LED as an output
+
+ servo.attach(9); //Servo attached to pin 9 digital
 }
 
 void loop(){
   value = analogRead(pResistor);
   
   //You can change value "25"
-  if (value < 50 and value >25){
-    servo.write(100);  //Turn le)d off
+  if (value < 50 and value >25){ //There is MEDIUM LIGHT
+    servo.write(100);  //Turn LED off
+    digitalWrite(pinLED,HIGH); 
   }
-  else if(value < 25){
+  else if(value < 25){ // There is ALMOST NO LIGHT
      servo.write(180);
+    digitalWrite(pinLED,HIGH); 
   }
   else{
-     servo.write(0);
+     servo.write(0); // There is HIGH LIGHT
+    digitalWrite(pinLED,LOW); 
   }
 
-  delay(50); //Small delay
+  delay(50); //Small delay to save 
+
+  
+  
 }
